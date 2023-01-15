306d416fcac2ae6612e601a8a75ea15c77eafeee
vlad <vnelipovich63@gmail.com>
2023-01-15 15:15:37 +0300

    import/export 2 components

---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Technologies = () => {
+    return (
+        <div>
+            <a href="">Home</a>
+            <a href="">News Feed</a>
+            <a href="">Messages</a>
+        </div>
+    )
+}
+
+export default Technologies;
