bea3401207e898e1bc656f927a139ad2e629994c
vlad <vnelipovich63@gmail.com>
2023-02-01 23:32:44 +0300

    Breakdown into components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://i0.wp.com/antennadaily.ru/wp-content/uploads/2020/01/photo-1515238152791-8216bfdf89a7.jpeg?fit=1052%2C700&ssl=1"
+                    alt=""/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default Profile
