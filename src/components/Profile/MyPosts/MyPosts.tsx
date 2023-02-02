bffa9adbea1ded16108931b5f94bd9c69568acf2
vlad <vnelipovich63@gmail.com>
2023-02-02 23:28:09 +0300

    Did first props in posts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
+
+
 const MyPosts = () => {
     return (
         <div>
@@ -11,10 +13,8 @@ const MyPosts = () => {
                 <button>Remove</button>
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message='Hi, how are you?'/>
+                <Post message="It's my first post"/>
             </div>
         </div>
 
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import s from './Post.module.css'
 
-const Post = () => {
+
+type MyPostsTypeProps = {
+    message: string
+}
+
+const Post : React.FC<MyPostsTypeProps> = (props) => {
     return (
         <div className={s.item}>
             <img
                 src="https://bipbap.ru/wp-content/uploads/2022/11/1652235714_41-kartinkin-net-p-prikolnie-kartinki-dlya-stima-44.jpg"
                 alt=""/>
-            post1
+            {props.message}
             <div><span>Like</span></div>
         </div>
     )
