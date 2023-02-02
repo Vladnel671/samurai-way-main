197811eb619ca271a5eb6f817cae4541d3eb1652
vlad <vnelipovich63@gmail.com>
2023-02-02 21:48:46 +0300

    added css modules

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import s from  './Profile.module.css'
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img
                     src="https://i0.wp.com/antennadaily.ru/wp-content/uploads/2020/01/photo-1515238152791-8216bfdf89a7.jpeg?fit=1052%2C700&ssl=1"
@@ -16,11 +16,11 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className={s.posts}>
+                    <div className={s.item}>
                         post1
                     </div>
-                    <div>
+                    <div className={s.item}>
                         post2
                     </div>
                 </div>
