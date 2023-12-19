9094f28bd031b86243bcb8b0cebaeac888c42f74
vlad <vnelipovich63@gmail.com>
2023-12-19 18:19:52 +0300

    feat: Implemented Follow/Unfollow functionality

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {UserType} from "../../types";
+import s from "./Users.module.css"
 
 type UsersPropsType = {
     users: UserType[]
@@ -9,12 +10,20 @@ type UsersPropsType = {
 }
 
 const Users: FC<UsersPropsType> = ({users, setUsers, onUnfollow, onFollow}) => {
+
+    const unFollow = (userID: number) => {
+        onUnfollow(userID)
+    }
+    const follow = (userID: number) => {
+        onFollow(userID)
+    }
+
     return (
-        <div>
+        <div className={s.UsersBlock}>
             {users.map(u => <span>
                 <div>
-                    <button>Follow</button>
-                    <button>UnFollow</button>
+                    {u.followed ? <button onClick={() => unFollow(u.id)}>Unfollow</button> :
+                        <button onClick={() => follow(u.id)}>Follow</button>}
                 </div>
                 <span>
                 {u.fullName}
